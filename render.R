dd6cbce14d1cb82197c6e987bdce899cca696c65
Tom Palmer <remlapmot@hotmail.com>
2024-07-11 19:36:33 +0100

    Update render.R

--- render.R
+++ render.R
@@ -9,6 +9,7 @@ reprex::reprex({
   try(quarto::quarto_render('tbl-cross-reprex.qmd', output_file = 'tbl-cross-reprex-gt-new.html'))
 })
 rmarkdown::render('tbl-cross-reprex-rmd.Rmd', output_file = 'tbl-cross-reprex-rmd-gt-new.html')
+try(quarto::quarto_render('tbl-cross-reprex-md.qmd', output_file = 'tbl-cross-reprex-md-gt-new.html'))
 
 # Current gt and pre-release Quarto
 quarto::quarto_render('tbl-cross-reprex.qmd', output_file = 'tbl-cross-reprex-gt-new-quarto-pre.html')
